a013f26ff4ceead6b22b0b1ae23a61acab520da1
Pierrick Bignet <pierrick.bignet@gmail.com>
2025-06-13 16:32:40 +0200

    🔥 remove MikroORM forFeature as they are not required in our case

--- apps/api/src/modules/comments/comments.module.ts
+++ apps/api/src/modules/comments/comments.module.ts
@@ -1,13 +1,8 @@
-import { MikroOrmModule } from '@mikro-orm/nestjs'
 import { Module } from '@nestjs/common'
 import { CommentsController } from './comments.controller'
-import { Comment } from './comments.entity'
 import { CommentsService } from './comments.service'
 
 @Module({
-  imports: [
-    MikroOrmModule.forFeature([Comment]),
-  ],
   controllers: [CommentsController],
   providers: [CommentsService],
   exports: [CommentsService],
--- apps/api/src/modules/posts/posts.module.ts
+++ apps/api/src/modules/posts/posts.module.ts
@@ -1,13 +1,8 @@
-import { MikroOrmModule } from '@mikro-orm/nestjs'
 import { Module } from '@nestjs/common'
 import { PostController, PublicPostController } from './posts.controller'
-import { Post, PostVersion } from './posts.entity'
 import { PostService } from './posts.service'
 
 @Module({
-  imports: [
-    MikroOrmModule.forFeature([Post, PostVersion]),
-  ],
   controllers: [PostController, PublicPostController],
   providers: [PostService],
   exports: [PostService],
