672982308a31a783ec067432af762d4b162ad55b
Pierrick Bignet <pierrick.bignet@gmail.com>
2025-11-24 17:40:46 +0100

    🚀 test auto deploy

--- apps/documentation/astro.config.mjs
+++ apps/documentation/astro.config.mjs
@@ -4,9 +4,11 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lonestone.github.io',
+  base: '/lonestone-boilerplate',
   integrations: [
     starlight({
-      title: 'My Docs',
+      title: 'Lonestone Boilerplate Documentation',
       social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
       sidebar: [
         { slug: 'quickstart' },
