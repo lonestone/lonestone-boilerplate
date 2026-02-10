7850667ef4fee2219f94171b3f226d726abec6e2
Netoun <coulonnier.nicolas.44@gmail.com>
2026-02-10 14:39:01 +0100

    Migrate nest from tsx to swc and jest to vitest

--- apps/api/src/modules/example/posts/posts.module.ts
+++ apps/api/src/modules/example/posts/posts.module.ts
@@ -1,8 +1,11 @@
+import { MikroOrmModule } from '@mikro-orm/nestjs'
 import { Module } from '@nestjs/common'
 import { PostController, PublicPostController } from './posts.controller'
+import { Post, PostVersion } from './posts.entity'
 import { PostService } from './posts.service'
 
 @Module({
+  imports: [MikroOrmModule.forFeature([Post, PostVersion])],
   controllers: [PostController, PublicPostController],
   providers: [PostService],
   exports: [PostService],
